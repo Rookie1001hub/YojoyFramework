d4a4481137e958c2a3e54a6159eb7ae91c2e2eeb
Rookie1001hub <1347884747@qq.com>
2020-05-20 23:09:48 +0800

    Csharp辅助类

---
+++ Assets/YojoyFramework/FunctionPackages/CorePackages/Yojoy.Tech.Common.Core/CsharpSrc/Editor/ScriptCreat/IfPreCompileBlock.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace Yojoy.Tech.Common.Core.Editor
+{
+    public class IfPreCompileBlock : IDisposable
+    {
+        private readonly CsharpScriptAppender appender;
+        private readonly List<string> instructions;
+        public IfPreCompileBlock(CsharpScriptAppender appender,
+            List<string> instruction)
+        {
+            this.appender = appender;
+            this.instructions = instruction;
+            AppendIfStart();
+        }
+        private void AppendIfStart()
+        {
+            for (int index = 0; index < instructions.Count; index++)
+            {
+                var instruct = instructions[index];
+                appender.Append(index == 0
+                    ? $"#if{instruct}"
+                    : $"&&{instruct}");
+            }
+        }
+
+        public void Dispose()
+        {
+            if (instructions == null || instructions.Count == 0)
+                return;
+            appender.AppendLine();
+            appender.AppendLine("#endif");
+        }
+    }
+}
+
+
---
+++ Assets/YojoyFramework/FunctionPackages/CorePackages/Yojoy.Tech.Common.Core/CsharpSrc/Editor/ScriptCreat/NameSpaceBlock.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Yojoy.Tech.Common.Core.Editor
+{
+    public class NameSpaceBlock : IDisposable
+    {
+        private readonly CsharpScriptAppender appender;
+        
+        public NameSpaceBlock(CsharpScriptAppender appender,
+            string @namespace)
+        {
+            this.appender = appender;
+            appender.AppendLine($"namespace{@namespace}");
+            appender.AppenLeftBracketAndToRight();
+        }
+
+        public void Dispose() => appender.AppendToLeftAndRightBracket();
+       
+    }
+}
+
